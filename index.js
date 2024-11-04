5fdfcc7b7712b6033e35e74ccd94e06a5efb03ae
GabriellaFS <gabriella.feitosadesalive@hotmail.com>
2024-11-04 07:24:32 -0300

    o projeto do Felipao foi muito legal de fazer

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("nós de novo")
+
+let nomeHeroi = " Ana Julia "
+let  xp = 6000
+
+if ( xp <= 1000){
+
+    console.log(" O Herói de nome" + nomeHeroi,"é de nível Ferro" )
+    
+   }else if ( xp >= 1001 &&  xp <= 2000){
+
+     console.log("  O Herói de nome"+ nomeHeroi, "é de nível Bronze ")
+}
+
+{
+     if ( xp >= 2001 && xp <= 5000){
+      console.log("O Herói de nome"+ nomeHeroi, "é de nível Prata")
+}
+
+   else if (xp >= 5001 &&  xp<=7000 ){
+
+    console.log("O Herói de nome"+ nomeHeroi, "é de nível Ouro")
+}
+
+    if( xp >= 7001 && xp <= 8000 ){
+
+    console.log("O Herói de nome"+ nomeHeroi, "é de nível Platina")
+}
+
+  else if(xp >= 8001 && xp <= 9000){
+
+    console.log("O Herói de nome"+ nomeHeroi, "é de nível Ascendente")
+}
+
+ 
+    if( xp >= 9001 && xp <= 10000){
+
+        console.log("O Herói de nome"+ nomeHeroi, "é de nível Imortal")
+    }
+
+  else if( xp >= 10001 ){
+    console.log("O Herói de nome"+ nomeHeroi, "é de nível Radiante")
+  }
+
+ 
+}
